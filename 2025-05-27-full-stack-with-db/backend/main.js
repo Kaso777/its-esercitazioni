7d1fe4b395db27bbb84868eaa5204ad15511c0b0
KasoLinux <laMiaEmail>
2025-05-27 12:43:09 +0200

    initial commit docker

--- 2025-05-27-full-stack-with-db/backend/main.js
+++ 2025-05-27-full-stack-with-db/backend/main.js
@@ -1,1 +1,26 @@
-console.log("Ho messo la sicura alla vecchia Betsy")
+console.log("Funzionaaa! Ho inventato qualcosa che funziona!!!");
+
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MySQL
+const db = mysql.createConnection({
+  host: 'localhost',       // or '127.0.0.1'
+  user: 'myuser',
+  password: 'mypass',
+  database: 'mydb',
+});
+
+// Test connection
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL connection error:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
